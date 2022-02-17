0dcbb38afff09f4814e318660b644ee378db5f98
dockraspberrypi <unaussprechlich@yahoo.com>
2022-02-17 23:43:35 +0100

    add --auto-zoom when displaying a picture so we see as much as possible

--- initpicture.sh
+++ initpicture.sh
@@ -6,6 +6,6 @@ CNT=`ls -l /home/pi/smartframe | wc -l`
 if [ $CNT -ge 1 ]
 then
    FILE=`ls -1 /home/pi/smartframe/*jpeg | head -1 `
-   /usr/bin/feh -F -x -Y --scale-down $FILE &
+   /usr/bin/feh -F -x -Y --scale-down --auto-zoom $FILE &
 fi
 

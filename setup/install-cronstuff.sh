241714c8bbaac70c5e23f2025b6ac77144f7acc0
christopher dock <unaussprechlich@yahoo.com>
2022-07-04 00:14:29 +0200

    add pi crontab

--- setup/install-cronstuff.sh
+++ setup/install-cronstuff.sh
@@ -3,3 +3,6 @@
 cp root/checknet.sh /root
 chmod 755 /root/checknet.sh 
 cat root/crontab.root >> /var/spool/cron/crontabs/root
+
+cat pi/crontab.pi >> /var/spool/cron/crontabs/pi
+chown pi /var/spool/cron/crontabs/pi
